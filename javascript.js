9e1baceb5b54903cb76ebeb1e17b76c563221ef7
Silas Matos <silas.progamacao@gmail.com>
2022-09-07 13:50:19 -0300

    project web

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function img01() {
+  document.getElementById('trocarimg').src = '/projeto/imagens/cardimg1.png'
+}
+function img02() {
+  document.getElementById('trocarimg').src = '/projeto/imagens/cardimg3.png'
+}
+function img03() {
+  document.getElementById('trocarimg').src = '/projeto/imagens/cardimg2.png'
+}
